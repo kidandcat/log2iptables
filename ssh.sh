7752fe0b1a58bbfe2384ff40763b3fa01a045801
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-12-05 12:22:25 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 wget -P /usr/local/bin/ https://raw.githubusercontent.com/kidandcat/log2iptables/master/log2iptables.sh
-(crontab -l 2>/dev/null; echo "*/5 * * * * /usr/local/bin/log2iptables.sh -x 1 -f /var/log/auth.log -r "sshd.*(f|F)ail.*(\=| )([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})" -p 3 -l 5 -m "kidandcat@gmail.com" -M "noreply@galax.be" > /dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * /usr/local/bin/log2iptables.sh -x 1 -f /var/log/auth.log -r \"sshd.*(f|F)ail.*(\=| )([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\" -p 3 -l 5 -m \"kidandcat@gmail.com\" -M \"noreply@galax.be\" > /dev/null 2>&1") | crontab -
